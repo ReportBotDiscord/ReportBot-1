00a6c395973a76569bf8742db1c3b9c57c05e544
CraZeBot <33512180+CraZeBot@users.noreply.github.com>
2017-12-28 13:14:56 +0000

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ client.on('message', message => {
 		if (message.content.startsWith(prefix + `@`)) {
 		message.reply('Thank You For Using Our Report System! You Will Shortly Get A DM From Staff!');
 	console.log(`A User Was Reported`);
-  });
 	} else
 
 	if (message.content.startsWith(`-say`)) {
