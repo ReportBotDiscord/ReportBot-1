2fcf40691a7847b577033a0206d83652227bee2a
CraZeBot <33512180+CraZeBot@users.noreply.github.com>
2017-12-28 20:14:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -48,14 +48,4 @@ client.on('message', message => {
 		message.reply('Thank You For Using Our Report System! You Will Shortly Get A DM From Staff!');
 	} else
 
-	if (message.content.startsWith(`say`)) {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
-
 client.login(process.env.BOT_TOKEN);
