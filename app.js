45239c511c2d3e90e07b6cffa6fe2c47f2d7fe8b
CraZeBot <33512180+CraZeBot@users.noreply.github.com>
2017-12-28 20:46:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ client.on('message', message => {
 		}
 
 		if (message.content.startsWith(prefix + ``)) {
-		message.reply(`Thanks For Using Our Report Sytem!, You Will Shortly Get A DM From A Staff Member! You Report Number Is #${number}`);
+		message.reply(`Thanks For Using Our Report Sytem!, You Will Shortly Get A DM From A Staff Member! Your Report Number Is #${number}`);
 	} else
 
 	if (message.content.startsWith(`say`)) {
