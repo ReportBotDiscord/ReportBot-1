3919e651b17e1f224faebc4cce5d7074567a42e9
CraZeBot <33512180+CraZeBot@users.noreply.github.com>
2017-12-30 01:26:49 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const number = Math.floor(Math.random() * 9999) + 1;
-const roll = Math.floor(Math.random() * 6) + 1;
 
 client.on("ready", () => {
     client.user.setGame(`-report [@name] [reason]`);
     console.log(`${client.guilds.size} Servers,  ${client.users.size} Users, \n Online!`);
-});
+  });
 
-// Join Messages
+// Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find('name', 'joins');
+  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
+  // Send the message, mentioning the member
   channel.send(`Everyone please welcome our newest member, ${member}`);
 });
 
@@ -31,19 +33,19 @@ client.on('message', message => {
 
 	if (!message.content.startsWith(prefix)) return;
 	if (message.author.bot) return;
-	
-client.on('message', message => {
-	if (message.content === '-ping') {
+
+	if (message.content.startsWith(prefix2 + `ping`)) {
 		message.channel.send('Pinging...').then(sent => {
 			sent.edit(`Pong!\nTook \`${(sent.createdTimestamp - message.createdTimestamp) / 2}\`ms`)
 		  })
 		}
-                 // -report sytem
+
 		if (message.content.startsWith(prefix + ``)) {
 			const number = Math.floor(Math.random() * 9999) + 1;
-		message.reply(`Thanks For Using Our Report Sytem!, You Will Shortly Get A DM From A Staff Member! You Report Number Is **#${number}**`);
+		message.reply(`Thanks For Using Our Report Sytem!, You Will Shortly Get A DM From A Staff Member! You Report Number Is #${number}`);
 	} else
-			if (message.content.startsWith(`say`)) {
+
+	if (message.content.startsWith(`say`)) {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
@@ -59,5 +61,3 @@ client.on('message', message => {
 });
 
 client.login(process.env.BOT_TOKEN);
-	
-});
